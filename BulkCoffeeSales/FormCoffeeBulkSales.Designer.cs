09ccdee7cb156f0cf77d412d67d75298600e818c
Anju Chawla <anju.chawla@georgiancollege.ca>
2016-11-09 09:25:51 -0500

    Added code

---
+++ BulkCoffeeSales/FormCoffeeBulkSales.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace BulkCoffeeSales
+{
+    partial class FormCoffeeBulkSales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCoffeeBulkSales));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBoxCoffeePrices = new System.Windows.Forms.GroupBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.comboBoxCoffeeType = new System.Windows.Forms.ComboBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonPrint = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonFind = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBoxQuantity = new System.Windows.Forms.GroupBox();
+            this.radioButtonFullPound = new System.Windows.Forms.RadioButton();
+            this.radioButtonHalfPound = new System.Windows.Forms.RadioButton();
+            this.radioButtonQuarterPound = new System.Windows.Forms.RadioButton();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.groupBoxCoffeePrices.SuspendLayout();
+            this.groupBoxQuantity.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(145, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bulk Coffee Sales";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(413, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Anju Chawla";
+            // 
+            // groupBoxCoffeePrices
+            // 
+            this.groupBoxCoffeePrices.Controls.Add(this.textBoxPrice);
+            this.groupBoxCoffeePrices.Controls.Add(this.comboBoxCoffeeType);
+            this.groupBoxCoffeePrices.Controls.Add(this.buttonExit);
+            this.groupBoxCoffeePrices.Controls.Add(this.buttonPrint);
+            this.groupBoxCoffeePrices.Controls.Add(this.buttonClear);
+            this.groupBoxCoffeePrices.Controls.Add(this.buttonFind);
+            this.groupBoxCoffeePrices.Controls.Add(this.label4);
+            this.groupBoxCoffeePrices.Controls.Add(this.label3);
+            this.groupBoxCoffeePrices.Controls.Add(this.groupBoxQuantity);
+            this.groupBoxCoffeePrices.Location = new System.Drawing.Point(35, 83);
+            this.groupBoxCoffeePrices.Name = "groupBoxCoffeePrices";
+            this.groupBoxCoffeePrices.Size = new System.Drawing.Size(441, 234);
+            this.groupBoxCoffeePrices.TabIndex = 2;
+            this.groupBoxCoffeePrices.TabStop = false;
+            this.groupBoxCoffeePrices.Text = "Coffee Prices";
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(277, 116);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.ReadOnly = true;
+            this.textBoxPrice.Size = new System.Drawing.Size(121, 22);
+            this.textBoxPrice.TabIndex = 8;
+            this.textBoxPrice.TabStop = false;
+            // 
+            // comboBoxCoffeeType
+            // 
+            this.comboBoxCoffeeType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCoffeeType.FormattingEnabled = true;
+            this.comboBoxCoffeeType.Items.AddRange(new object[] {
+            "Regular",
+            "Decaffeinated",
+            "Special Blend"});
+            this.comboBoxCoffeeType.Location = new System.Drawing.Point(277, 70);
+            this.comboBoxCoffeeType.Name = "comboBoxCoffeeType";
+            this.comboBoxCoffeeType.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxCoffeeType.TabIndex = 2;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(340, 184);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 30);
+            this.buttonExit.TabIndex = 6;
+            this.buttonExit.Text = "E&xit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonPrint
+            // 
+            this.buttonPrint.Location = new System.Drawing.Point(233, 184);
+            this.buttonPrint.Name = "buttonPrint";
+            this.buttonPrint.Size = new System.Drawing.Size(75, 30);
+            this.buttonPrint.TabIndex = 5;
+            this.buttonPrint.Text = "&Print";
+            this.buttonPrint.UseVisualStyleBackColor = true;
+            this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(126, 184);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 30);
+            this.buttonClear.TabIndex = 4;
+            this.buttonClear.Text = "&Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.Location = new System.Drawing.Point(19, 184);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(75, 30);
+            this.buttonFind.TabIndex = 3;
+            this.buttonFind.Text = "&Find Price";
+            this.buttonFind.UseVisualStyleBackColor = true;
+            this.buttonFind.Click += new System.EventHandler(this.buttonFind_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(225, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "&Type";
+            // 
+            // groupBoxQuantity
+            // 
+            this.groupBoxQuantity.Controls.Add(this.radioButtonFullPound);
+            this.groupBoxQuantity.Controls.Add(this.radioButtonHalfPound);
+            this.groupBoxQuantity.Controls.Add(this.radioButtonQuarterPound);
+            this.groupBoxQuantity.Location = new System.Drawing.Point(19, 44);
+            this.groupBoxQuantity.Name = "groupBoxQuantity";
+            this.groupBoxQuantity.Size = new System.Drawing.Size(165, 118);
+            this.groupBoxQuantity.TabIndex = 0;
+            this.groupBoxQuantity.TabStop = false;
+            this.groupBoxQuantity.Text = "Quantity";
+            // 
+            // radioButtonFullPound
+            // 
+            this.radioButtonFullPound.AutoSize = true;
+            this.radioButtonFullPound.Location = new System.Drawing.Point(22, 80);
+            this.radioButtonFullPound.Name = "radioButtonFullPound";
+            this.radioButtonFullPound.Size = new System.Drawing.Size(96, 21);
+            this.radioButtonFullPound.TabIndex = 2;
+            this.radioButtonFullPound.TabStop = true;
+            this.radioButtonFullPound.Text = "Fu&ll Pound";
+            this.radioButtonFullPound.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonHalfPound
+            // 
+            this.radioButtonHalfPound.AutoSize = true;
+            this.radioButtonHalfPound.Location = new System.Drawing.Point(22, 53);
+            this.radioButtonHalfPound.Name = "radioButtonHalfPound";
+            this.radioButtonHalfPound.Size = new System.Drawing.Size(99, 21);
+            this.radioButtonHalfPound.TabIndex = 1;
+            this.radioButtonHalfPound.TabStop = true;
+            this.radioButtonHalfPound.Text = "&Half Pound";
+            this.radioButtonHalfPound.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonQuarterPound
+            // 
+            this.radioButtonQuarterPound.AutoSize = true;
+            this.radioButtonQuarterPound.Location = new System.Drawing.Point(22, 26);
+            this.radioButtonQuarterPound.Name = "radioButtonQuarterPound";
+            this.radioButtonQuarterPound.Size = new System.Drawing.Size(123, 21);
+            this.radioButtonQuarterPound.TabIndex = 0;
+            this.radioButtonQuarterPound.Text = "&Quarter Pound";
+            this.radioButtonQuarterPound.UseVisualStyleBackColor = true;
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // FormCoffeeBulkSales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 362);
+            this.Controls.Add(this.groupBoxCoffeePrices);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormCoffeeBulkSales";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "R \'n R for Reading and Refreshment";
+            this.Load += new System.EventHandler(this.FormCoffeeBulkSales_Load);
+            this.groupBoxCoffeePrices.ResumeLayout(false);
+            this.groupBoxCoffeePrices.PerformLayout();
+            this.groupBoxQuantity.ResumeLayout(false);
+            this.groupBoxQuantity.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBoxCoffeePrices;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.ComboBox comboBoxCoffeeType;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonFind;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBoxQuantity;
+        private System.Windows.Forms.RadioButton radioButtonFullPound;
+        private System.Windows.Forms.RadioButton radioButtonHalfPound;
+        private System.Windows.Forms.RadioButton radioButtonQuarterPound;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ BulkCoffeeSales/FormCoffeeBulkSales.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BulkCoffeeSales
+{
+      public partial class FormCoffeeBulkSales : Form
+    {
+        //module level variables
+        private struct CoffeeSale
+        {
+            public String quantity;
+            public String type;
+            public decimal price;
+        };
+        //number of transactions to save
+        private const int MaximumTransactions = 5;
+        //save the transactions in an array
+        private CoffeeSale[] transactionsCoffeeSales = new CoffeeSale[MaximumTransactions];
+        //save thhe transactions in a list
+        private List<CoffeeSale> transactionsCoffeeSalesList = new List<CoffeeSale>();
+        private string selectedRadioButton;
+        private int transactionNumber;
+
+        public FormCoffeeBulkSales()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            /*
+
+            //setting up the print page for preview and print
+            Font printFont = new Font("Arial", 12);
+            Font headingFont = new Font("Arial Black", 14, FontStyle.Bold);
+            float lineHeightFloat = printFont.GetHeight();
+            float col1HorizontalPrintLocationFloat = e.MarginBounds.Left;
+            float col2HorizontalPrintLocationFloat = 300;
+            float col3HorizontalPrintLocationFloat;
+            float verticalPrintLocationFloat = e.MarginBounds.Top;
+            SizeF fontSizeF = new SizeF();
+            string formattedPriceString;
+
+
+            //print the heading
+            e.Graphics.DrawString("Coffee Bulk Sales Report", headingFont, Brushes.Black,
+                col2HorizontalPrintLocationFloat, verticalPrintLocationFloat);
+            verticalPrintLocationFloat += lineHeightFloat * 2;
+            //headings to columns
+            e.Graphics.DrawString("Quantity", headingFont, Brushes.Black,
+               col1HorizontalPrintLocationFloat, verticalPrintLocationFloat);
+            e.Graphics.DrawString("Type", headingFont, Brushes.Black,
+               col2HorizontalPrintLocationFloat, verticalPrintLocationFloat);
+            e.Graphics.DrawString("Price", headingFont, Brushes.Black,
+               500, verticalPrintLocationFloat);
+
+            verticalPrintLocationFloat += lineHeightFloat * 2;
+
+            //print the transactions
+            // can use a for each loop
+            foreach (CoffeeSale transaction in transactionsCoffeeSalesList)
+            {
+                //refer with transaction.quantityString
+                //print the quantity
+                e.Graphics.DrawString(transaction.quantityString, printFont,
+                    Brushes.Black, col1HorizontalPrintLocationFloat, verticalPrintLocationFloat);
+                //print the coffee type
+                e.Graphics.DrawString(transaction.typeString, printFont,
+                   Brushes.Black, col2HorizontalPrintLocationFloat, verticalPrintLocationFloat);
+                //format and right align the price
+                formattedPriceString = transaction.priceDecimal.ToString("c");
+                //measure string in this fone
+                fontSizeF = e.Graphics.MeasureString(formattedPriceString, printFont);
+                //subtract width from column position
+                col3HorizontalPrintLocationFloat = 550 - fontSizeF.Width;
+                //print the price
+                e.Graphics.DrawString(formattedPriceString, printFont,
+                  Brushes.Black, col3HorizontalPrintLocationFloat, verticalPrintLocationFloat);
+
+                //go to next printing position
+                verticalPrintLocationFloat += lineHeightFloat;
+            }
+
+            for (int index = 0; index < transactionNumberInteger; index++)
+            {
+                //print the quantity
+                e.Graphics.DrawString(transactionsCoffeeSales[index].quantityString, printFont,
+                    Brushes.Black, col1HorizontalPrintLocationFloat, verticalPrintLocationFloat);
+                //print the coffee type
+                e.Graphics.DrawString(transactionsCoffeeSales[index].typeString, printFont,
+                   Brushes.Black, col2HorizontalPrintLocationFloat, verticalPrintLocationFloat);
+                //format and right align the price
+                formattedPriceString = transactionsCoffeeSales[index].priceDecimal.ToString("c");
+                //measure string in this fone
+                fontSizeF = e.Graphics.MeasureString(formattedPriceString, printFont);
+                //subtract width from column position
+                col3HorizontalPrintLocationFloat = 550 - fontSizeF.Width;
+                //print the price
+                e.Graphics.DrawString(formattedPriceString, printFont,
+                  Brushes.Black, col3HorizontalPrintLocationFloat, verticalPrintLocationFloat);
+
+                //go to next printing position
+                verticalPrintLocationFloat += lineHeightFloat;
+
+
+            }*/
+
+            }
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+            /*
+            //preview the transaction report and print if required
+            //print if there are any transactions
+            if (transactionNumberInteger > 0)
+            {
+                printPreviewDialog1.Document = printDocument1;
+                printPreviewDialog1.ShowDialog();
+                //only if printing is required without previewing
+                //printDocument1.Print();
+            }
+            else
+            {
+                MessageBox.Show("There are no transactions to print",
+                   "Print Transactions", MessageBoxButtons.OK,
+                   MessageBoxIcon.Information);
+            }
+            */
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            //terminate the application
+            Application.Exit();
+        }
+
+        private void FormCoffeeBulkSales_Load(object sender, EventArgs e)
+        {
+            //set default value
+            selectedRadioButton = "radioButtonQuarterPound";
+
+        }
+
+        private void buttonFind_Click(object sender, EventArgs e)
+        {
+            //find and display the price of the coffee based on the type and quantity chosen by the user
+
+            //save the prices of each coffee type for look up-each row is for a coffee type
+            //regular,decaffinated, special blend - quarter, half and full pound prices 
+            decimal[,] price = { 
+                {2.60m,2.90m,3.25m }, 
+                {4.90m,5.60m,6.10m}, 
+                {8.75m,9.75m,11.25m }
+            };
+            int row, column;            
+            decimal salesPrice;
+
+
+            //allow only certain number of transactions
+            if(transactionNumber  < MaximumTransactions )
+            {
+                //find the type of coffee selected
+                column = comboBoxCoffeeType.SelectedIndex;
+
+                //if coffee type selected
+                if(column != -1)
+                {
+
+
+                }//coffee type selected
+                else
+                {
+                    MessageBox.Show("Please select a coffee type", "Incomplete Selection",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    comboBoxCoffeeType.Focus();
+                }
+
+            }//maximum transactions
+            else  //cannot save more than maximum transactions
+            {
+                MessageBox.Show("Only "+ MaximumTransactions + " are allowed", "Maximum Transactions",
+                                      MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+    }
+}
